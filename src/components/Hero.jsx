028ea77d28b693d07f6be7de135d43c774cfc787
uralaleksandr <aleksandrural@hotmail.com>
2023-01-05 21:04:03 +0000

    feat: added hero component main paragraph

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -17,12 +17,21 @@ const Hero = () => (
       <div className="flex flex-row justify-between items-center w-full">
         <h1 className="flex-1 font-poppins font-semibold ss:text-[72px] text-[52px] text-white ss:leading-[100px] leading-[75px]">
           The Next <br className="sm:block hidden" />{" "}
-          <span className="text-gradient">Generation</span> Payment Method.
+          <span className="text-gradient">Generation</span>
         </h1>
         <div className="ss:flex hidden md:mr-4 mr-0">
           <GetStarted />
         </div>
       </div>
+
+      <h1 className="font-poppins font-semibold ss:text-[68px] text-[52px] text-white ss:leading-[100px] leading-[75px] w-full">
+        Payment Method.
+      </h1>
+      <p className={`${styles.paragraph} max-w-[470px] mt-5 `}>
+        Our team of experts use a complex methodology to identify the credit
+        cards most likely to fit your needs. We examine annual percentage rates,
+        annual fees, and much more.
+      </p>
     </div>
   </section>
 );
